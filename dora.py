1d91b2cbc864701c1443b6718a9e6544ab9e95bc
Bhumika Vijayakumar <bhumikavijaykumar11@gmail.com>
2020-04-07 14:42:04 +0530

    fixed pdf

--- dora.py
+++ dora.py
@@ -37,6 +37,7 @@ def echo(bot, update):
                         update.effective_message.reply_text(dat['doi'])
                     sci = SciHub(dat['doi'], out='output').download(choose_scihub_url_index=0)
                     bot.send_document(chat_id=update.effective_message.chat_id, document=open('output/'+dat['title']+'.pdf', 'rb'))
+                    update.effective_message.reply_text(dat['doi'])
             except Exception as e:
                 pass
     else:
